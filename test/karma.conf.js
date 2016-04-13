0a6ad16c96b739fcf76fda831c4d17e62e57237b
David Klassen <f0rk.tt@gmail.com>
2016-04-13 12:16:02 +0200

    Added testing framework

---
+++ test/karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+        frameworks: ['mocha', 'browserify'],
+        files: [
+            'dist/index.js',
+            'test/**/*.test.js'
+        ],
+        preprocessors: {
+            'test/**/*.test.js': ['browserify']
+        },
+        browserify: {
+            debug: true,
+            transform: [['babelify', { 'presets': ['es2015'] } ]]
+        },
+        reporters: ['spec'],
+        colors: true,
+        browsers: ['PhantomJS'],
+        singleRun: true
+    });
+};
---
+++ test/utils/post.test.js
@@ -0,0 +1,5 @@
+describe('ololo', () => {
+    it('should be ololololish', () => {
+        
+    });
+});
