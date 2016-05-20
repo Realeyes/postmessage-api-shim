2a987b84953bd76c006c87ab5a96eee4df4570e5
David Klassen <f0rk.tt@gmail.com>
2016-05-20 11:32:51 +0200

    Updated header comments

--- build.js
+++ build.js
@@ -1,3 +1,10 @@
+/**
+ * Entry point for browserify. Used to build a version for browser.
+ * Exports postmessage-api-shim functions to the window namespace.
+ *
+ * @copyright Realeyes OU. All rights reserved.
+ */
+
 import * as pas from './index';
 
 window.pas = pas;
--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+/**
+ * Export public interface.
+ *
+ * @copyright Realeyes OU. All rights reserved.
+ */
+
 export { CreateServer } from './lib/rpc/server';
 export { CreateClient, ReleaseClient } from './lib/rpc/client';
